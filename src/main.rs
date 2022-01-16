2c1fc7739e80c6452b57cf86a23a3e853d077776
Yuguo Zheng <zhenghugo515@gmail.com>
2022-01-16 16:42:29 -0500

    First step of DSA for identifier

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+enum State {
+    A,
+    B,
+}
+
+struct Machine {
+    state: State,
+}
+
+impl Machine {
+    fn transit(&mut self, input: char) {
+        self.state = match self.state {
+            State::A => match input {
+                'A'..='Z' | 'a'..='z' => State::B,
+                _ => panic!("Invalid input!"),
+            },
+            State::B => match input {
+                'A'..='Z' | 'a'..='z' | '0'..='9' | '_' => State::B,
+                _ => panic!("Invalid input!"),
+            },
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
