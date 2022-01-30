c7842d7a2f018da2f11321aa6c7c30b29e44ac50
Yuguo Zheng <zhenghugo515@gmail.com>
2022-01-29 21:09:12 -0500

    Add test cases

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # COMP 442/6421 (Compiler Design) Winter 2022 at Concordia University - Course Project
 ### A compiler frontend prototype, implemented in Rust
+See the "doc" directory for detailed documentation. 
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,39 @@ use crate::lexer::Lexer;
 use crate::lexer_machine_impl::LexerStateMachineImpl;
 use crate::lexer_machine_impl::State;
 use crate::lexical_error::LexicalError;
-use crate::token::Token;
+use crate::token::{Token, TokenType};
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 
-fn main() {
-    let input_files = ["lexnegativegrading", "lexpositivegrading"];
-    for input_file in input_files {
-        let source: String = fs::read_to_string(input_file.to_owned() + ".src")
+fn main() -> std::io::Result<()> {
+    let input_files_prefix = ["sample", "lexnegativegrading", "lexpositivegrading"];
+    for input_file_prefix in input_files_prefix {
+        let source: String = fs::read_to_string(input_file_prefix.to_owned() + ".src")
             .expect("Something went wrong reading the file");
+        let mut out_lex_tokens_file = File::create(input_file_prefix.to_owned() + ".outlextokens")?;
+        let mut out_lex_errors_file = File::create(input_file_prefix.to_owned() + ".outlexerrors")?;
         let mut lexer: Lexer = Lexer::new();
         lexer.read_source(&source);
-        loop {
-            let token = lexer.next_token();
-            if token.is_none() {
-                break;
+        while let Some(token) = lexer.next_token() {
+            out_lex_tokens_file
+                .write_all((token.clone().to_string() + "\n").as_bytes())
+                .expect("Something went wrong writing the file");
+            match token.token_type {
+                TokenType::InvalidTokenType(invalid_token_type) => {
+                    out_lex_errors_file
+                        .write_all(
+                            (format!(
+                                "Lexical error: {}: \"{}\": line {}.\n",
+                                invalid_token_type, token.lexeme, token.location.0
+                            ))
+                            .as_bytes(),
+                        )
+                        .expect("Something went wrong writing the file");
+                }
+                TokenType::ValidTokenType(_) => (),
             }
-            println!("{}", token.unwrap());
         }
     }
+    Ok(())
 }
